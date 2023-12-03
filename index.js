1bfb17880b8fc5642f333817f7915ced5050cc10
Karina Li <karina.yukting.li@gmail.com>
2023-12-02 23:44:13 -0500

    Required in the libraries

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
